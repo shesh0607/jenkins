ce62bda0957cc560c1541c9f738b04f782af1706
Shesh <Shesh@LAPTOP-INML28EB>
2022-11-19 14:19:27 +0530

    update

---
+++ src/test/java/scripts/Test4.java
@@ -0,0 +1,11 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 {
+	@Test
+	public void tc1() {
+		Reporter.log("Hello Shesh",true);
+	}
+}
