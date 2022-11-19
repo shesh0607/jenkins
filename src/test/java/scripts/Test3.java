0e70d4f61b152d05abc7703e6285271f2bc7e6cd
Shesh <Shesh@LAPTOP-INML28EB>
2022-11-19 11:11:54 +0530

    update

---
+++ src/test/java/scripts/Test3.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void tc4() {
+		Reporter.log("hello pythonselenium",true);
+	}
+
+}
